a74531e6f51333842dd5985dcba403d9e034a555
Allan Vigiani <vigianiallan@gmail.com>
2020-06-13 14:48:35 -0300

    random

--- App/script.js
+++ App/script.js
@@ -12,36 +12,41 @@ function screenChange() {
 
 screenChange();
 
-//Posições randomicas
-var positionX = Math.floor(Math.random() * width) - 90;
-var positionY = Math.floor(Math.random() * height) - 90;
 
-console.log(positionX, positionY)
+function randomPosition() {
+//Remover mosquito
+   if (document.getElementById('gnat')) {
+      document.getElementById('gnat').remove();
+   }
+
+//Posições randomicas
+   var positionX = Math.floor(Math.random() * width) - 90;
+   var positionY = Math.floor(Math.random() * height) - 90;
+
+   console.log(positionX, positionY)
 
 //Se cair coordenada 0
-positionX = positionX < 0 ? 0 : positionX;
-positionY = positionY < 0 ? 0 : positionY;
+   positionX = positionX < 0 ? 0 : positionX;
+   positionY = positionY < 0 ? 0 : positionY;
 
 //Criar o elemento HTML
+   var imgGnat = document.createElement('img');
 
-var imgGnat = document.createElement('img');
+   imgGnat.src = 'imagens/mosquito.png'
+   imgGnat.className = gnatSize() + ' ' + randomSide();
+   imgGnat.style.left = positionX + 'px';
+   imgGnat.style.top = positionY + 'px';
+   imgGnat.style.position = 'absolute';
+   imgGnat.id = 'gnat';
 
-imgGnat.src = 'imagens/mosquito.png'
-imgGnat.className = gnatSize();
-imgGnat.style.left = positionX + 'px';
-imgGnat.style.top = positionY + 'px';
-imgGnat.style.position = 'absolute';
+   document.body.appendChild(imgGnat);
 
-document.body.appendChild(imgGnat);
-
-console.log(gnatSize())
+}
 
 //Tamanho do mosquito 
 function gnatSize() {
 
    var size = Math.floor(Math.random() * 3); //de 0 até 2
-   
-   console.log(size)
 
    if(size === 0) {
       return 'gnat1';
@@ -54,3 +59,25 @@ function gnatSize() {
    }
 
 }
+
+//Lado do mosquito
+function randomSide() {
+
+   var side = Math.floor(Math.random() * 2); //de 0 até 1
+
+   console.log(side);
+   
+   if (side === 0) {
+      return 'sideA';
+   }else if (side === 1){
+      return 'sideB';
+   }else () => {
+      console.log('ERROR');
+   }
+
+}
+
+//Timer dos mosquitos
+setInterval(function () {
+   randomPosition();
+}, 1000);
